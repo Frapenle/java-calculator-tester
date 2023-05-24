196eaabe484827631d91a49a3860aa575bd573f1
francescotrudu <francescotrudu84@gmail.com>
2023-05-24 14:43:22 +0200

    Deleted duplicated test

--- src/test/java/org/main/java/employee/EmployeeTest.java
+++ src/test/java/org/main/java/employee/EmployeeTest.java
@@ -11,52 +11,38 @@ import org.junit.jupiter.api.Test;
 
 public class EmployeeTest {
 	static String name;
-    static String lastName;
-    static LocalDate dateOfBirth;
-    static String role;
-    static Employee e;
+	static String lastName;
+	static LocalDate dateOfBirth;
+	static String role;
+	static Employee e;
 
-    @BeforeAll
-    static void setup() throws Exception {
-        name = "Francesco";
-        lastName = "Trudu";
-        dateOfBirth = LocalDate.of(1988, 10, 15);
-        role = "employee";
-        e = new Employee(name, lastName, dateOfBirth, role);
-    }
-    
-    @Test
-    @DisplayName("Test costruttore")
-    public void employeeTest(){
-    	
-    }
-    
-    @Test
-    @DisplayName("Test nome vuoto")
-    public void newEmployeeTest(){
-    	name = "";
-       assumeTrue(name.isBlank());
-       assertThrows(Exception.class,
-    		   () -> e.setName(name),
-    		   "Deve sollevare Eccezione se il nome è vuoto");
-    }
-    @Test
-    @DisplayName("Test nome valido")
-    public void newEmployeeTest2(){
-       assumeTrue(name.isBlank());
-       assertThrows(Exception.class,
-    		   () -> e.setName(name),
-    		   "Deve sollevare Eccezione se il nome è vuoto");
-    }
-    
-    @Test
-    @DisplayName("Test nome valido")
-    public void newEmployeeTest21(){
-       assumeTrue(name.isBlank());
-       assertThrows(Exception.class,
-    		   () -> e.setName(name),
-    		   "Deve sollevare Eccezione se il nome è vuoto");
-    }
-    
+	@BeforeAll
+	static void setup() throws Exception {
+		name = "Francesco";
+		lastName = "Trudu";
+		dateOfBirth = LocalDate.of(1988, 10, 15);
+		role = "employee";
+		e = new Employee(name, lastName, dateOfBirth, role);
+	}
 
+	@Test
+	@DisplayName("Test costruttore")
+	public void employeeTest() {
+
+	}
+
+	@Test
+	@DisplayName("Test nome vuoto")
+	public void newEmployeeTest() {
+		name = "";
+		assumeTrue(name.isBlank());
+		assertThrows(Exception.class, () -> e.setName(name), "Deve sollevare Eccezione se il nome è vuoto");
+	}
+
+	@Test
+	@DisplayName("Test nome valido")
+	public void newEmployeeTest2() {
+		assumeTrue(name.isBlank());
+		assertThrows(Exception.class, () -> e.setName(name), "Deve sollevare Eccezione se il nome è vuoto");
+	}
 }
